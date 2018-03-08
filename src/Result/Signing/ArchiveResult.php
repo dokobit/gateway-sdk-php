632e6d41699a2f45306798dcf461a6f2762948fb
Rimas Kudelis <rq@akl.lt>
2018-03-08 11:33:27 +0200

    Added signing Archive request and response classes

---
+++ src/Result/Signing/ArchiveResult.php
@@ -0,0 +1,42 @@
+<?php
+namespace Isign\Gateway\Result\Signing;
+
+use Isign\Gateway\Result\ResultInterface;
+
+/**
+ * Result object for signing/{token}/archive response.
+ */
+class ArchiveResult implements ResultInterface
+{
+
+    const STATUS_OK = 'ok';
+
+    /** @var string response status */
+    private $status;
+
+    /**
+     * Fields expected in response
+     */
+    public function getFields(): array
+    {
+        return [
+            'status',
+        ];
+    }
+
+    /**
+     * Set status
+     */
+    public function setStatus(string $status): void
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * Get status
+     */
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+}
