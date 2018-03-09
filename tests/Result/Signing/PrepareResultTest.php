a1542ca868c638f9f8ae6213a02e51dee3cf0b12
Rimas Kudelis <rq@akl.lt>
2018-03-09 15:34:37 +0200

    Tests for signing/{token}/prepare query and result

---
+++ tests/Result/Signing/PrepareResultTest.php
@@ -0,0 +1,28 @@
+<?php
+namespace Isign\Gateway\Tests\Result\Signing;
+
+use Isign\Gateway\Result\Signing\PrepareResult;
+use Isign\Gateway\Tests\TestCase;
+use Isign\Gateway\Tests\Result\TestResultFieldsTrait;
+
+class PrepareResultTest extends TestCase
+{
+    private $method;
+
+    public function setUp()
+    {
+        $this->method = new PrepareResult();
+    }
+
+    use TestResultFieldsTrait;
+
+    public function expectedFields()
+    {
+        return [
+            ['status'],
+            ['dtbs'],
+            ['dtbsHash'],
+            ['algorithm'],
+        ];
+    }
+}
